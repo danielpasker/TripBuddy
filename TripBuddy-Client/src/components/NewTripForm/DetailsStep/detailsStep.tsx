6f636503cc6f825561e75fcdb3f8717aeed6ab4b
Daniel Pasker <danielpasker123@gmail.com>
2025-02-28 18:57:00 +0200

    add FormValueSelect component

--- TripBuddy-Client/src/components/NewTripForm/DetailsStep/detailsStep.tsx
+++ TripBuddy-Client/src/components/NewTripForm/DetailsStep/detailsStep.tsx
@@ -2,7 +2,6 @@ import {FC} from 'react';
 import {useFormContext} from 'react-hook-form';
 import {ArrowBack, FlightTakeoffRounded, PeopleOutlineRounded} from '@mui/icons-material';
 import {StyledButton} from '@components/common/StyledButton';
-import {FormDatePicker} from '@components/common/input/FormDatePicker';
 import {FormInput} from '@components/common/input/FormInput';
 import styles from './styles.module.scss';
 
@@ -20,7 +19,10 @@ const DetailsStep: FC<Props> = ({isPlanningTrip, onSubmit, onReturn}) => {
 
   return (
     <div className={styles.container}>
-      <h1>TODO: fill the details of the trip as shown in figma - use fromInput to update the values into the form</h1>
+      <h1>
+        TODO: fill the details of the trip as shown in figma - use FormInput/FormDatePicker/FormValueSelect to update
+        the values into the form
+      </h1>
       {/* Example of FormInput usage */}
       <FormInput
         className={styles.input}
@@ -31,8 +33,6 @@ const DetailsStep: FC<Props> = ({isPlanningTrip, onSubmit, onReturn}) => {
         placeholder="Participants"
         endDecorator={<PeopleOutlineRounded />}
       />
-      {/* Example of FormDatePicker usage */}
-      <FormDatePicker control={control} formKey="startDate" className={styles.input} />
       <div className={styles.actions}>
         <StyledButton className={styles.returnButton} startDecorator={<ArrowBack />} onClick={onReturn}>
           Return
---
+++ TripBuddy-Client/src/components/common/input/StyledSelect/styledSelect.tsx
@@ -0,0 +1,39 @@
+import {memo} from 'react';
+import {Option, Select, SelectProps, Typography, styled} from '@mui/joy';
+import {glassEffect} from '@styles/commonStyles';
+import styles from './styles.module.scss';
+
+const CustomSelect = styled(Select)({
+  ...glassEffect,
+  padding: '8px 16px',
+  borderRadius: 100,
+  borderColor: 'rgba(255, 255, 255, 0.7)',
+  '--Icon-color': 'white',
+  '--joy-palette-text-icon': 'white',
+});
+
+interface Props extends Omit<SelectProps<{}, boolean>, 'onChange'> {
+  options: string[];
+  inputLabel?: string;
+  onChange: (value: string) => void;
+}
+
+const StyledSelect = memo<Props>(({options, onChange, inputLabel, ...props}) => (
+  <div className={styles.container}>
+    {inputLabel && <Typography level="body-md">{inputLabel}</Typography>}
+    <CustomSelect
+      {...props}
+      onChange={(_e, newValue) => {
+        onChange(newValue as string);
+      }}>
+      {options.map(option => (
+        <Option key={option} value={option}>
+          {option}
+        </Option>
+      ))}
+    </CustomSelect>
+  </div>
+));
+
+export type {Props as StyledSelectProps};
+export {StyledSelect};
