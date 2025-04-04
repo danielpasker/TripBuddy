ebec72eefbe28b3147f1c454daca4f2a609d2438
RonyL02 <280202rony@gmail.com>
2025-04-05 00:09:43 +0300

    move  db initialization to a separate function

--- TripBuddy-Server/src/server.ts
+++ TripBuddy-Server/src/server.ts
@@ -1,50 +1,45 @@
-import express, {Express} from 'express';
+import express, { Express } from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import {json, urlencoded} from 'body-parser';
-import 'dotenv/config';
-import {postRouter} from '@routes/postsRoutes';
-import {commentRouter} from '@routes/commentsRoutes';
-import {userRouter} from '@routes/usersRoutes';
-import {authRouter} from '@routes/authRoutes';
-import {filesRouter} from '@routes/filesRoute';
-import {tripPlanRouter} from '@routes/tripPlanRoutes';
-import {setupSwagger} from './swaggerConfig';
+import { json, urlencoded } from 'body-parser';
+import { postRouter } from '@routes/postsRoutes';
+import { commentRouter } from '@routes/commentsRoutes';
+import { userRouter } from '@routes/usersRoutes';
+import { authRouter } from '@routes/authRoutes';
+import { filesRouter } from '@routes/filesRoute';
+import { tripPlanRouter } from '@routes/tripPlanRoutes';
+import { setupSwagger } from './swaggerConfig';
+import { Env, verifyEnvVariables } from '@env';
 
-const app = express();
+verifyEnvVariables();
 
-const db = mongoose.connection;
-db.once('open', () => console.log('Connected to TripBuddy database'));
-db.on('error', (error) => console.error(error));
+const initDB = async () => {
+  try {
+    await mongoose.connect(Env.DB_CONNECTION_URL);
+    console.log('connected to db');
+  } catch (error) {
+    console.error(`failed connecting to db: ${error}`);
+  }
+};
 
-app.use(json({limit: '50mb'}));
-app.use(cors());
-app.use(urlencoded({extended: true}));
-app.use(express.static('public'));
-app.use(express.static('front'));
-app.use('/posts', postRouter);
-app.use('/comments', commentRouter);
-app.use('/auth', authRouter);
-app.use('/users', userRouter);
-app.use('/files', filesRouter);
-app.use('/trip-plan', tripPlanRouter);
+export const initApp = async () => {
+  await initDB();
 
-setupSwagger(app);
+  const app = express();
 
-const initializeExpress = () =>
-  new Promise<Express>((resolve, reject) => {
-    if (!process.env.DB_CONNECT) {
-      reject('DB_CONNECT is not defined in .env file');
-    } else {
-      mongoose
-        .connect(process.env.DB_CONNECT)
-        .then(() => {
-          resolve(app);
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    }
-  });
+  app.use(json({ limit: '50mb' }));
+  app.use(cors());
+  app.use(urlencoded({ extended: true }));
+  app.use(express.static('public'));
+  app.use(express.static('front'));
+  app.use('/posts', postRouter);
+  app.use('/comments', commentRouter);
+  app.use('/auth', authRouter);
+  app.use('/users', userRouter);
+  app.use('/files', filesRouter);
+  app.use('/trip-plan', tripPlanRouter);
 
-export {initializeExpress};
+  setupSwagger(app);
+
+  return app;
+};
