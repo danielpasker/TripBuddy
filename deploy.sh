afbc0e6b81b6fca2dec9516d797e63f7b489e00e
RonyL02 <280202rony@gmail.com>
2025-04-26 17:32:13 +0300

    add deploy status messages

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 START_DIR=$(pwd)
+echo "pulling from git"
 git pull
 
 cd TripBuddy-Client
+echo "installing client dependencies..."
 npm i
+echo "building client..."
 npm run prod
 
 cd ../TripBuddy-Server 
+echo "installing server dependencies..."
 npm i
+echo "building server..."
 npm run prod
 
 cd "$START_DIR"
