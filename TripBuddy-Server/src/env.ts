a2dfda60c044e0c1c544c8911515b2084866e26c
dthv201 <gaya.vishna@gmail.com>
2025-04-26 20:13:29 +0300

    revert .env changes

--- TripBuddy-Server/src/env.ts
+++ TripBuddy-Server/src/env.ts
@@ -1,24 +1,17 @@
-import path from 'path';
 import dotenv from 'dotenv';
 
+dotenv.config();
 
-const ENV = process.env.NODE_ENV || 'development';
-
-
-dotenv.config({ path: path.resolve(process.cwd(), `.env.${ENV}.local`) });
-dotenv.config({ path: path.resolve(process.cwd(), `.env.${ENV}`) });
-
-
- type PeriodString = `${number}${'m' | 'd'}`;
+type PeriodString = `${number}${'m' | 'd'}`;
 
 export const Env = {
-  NODE_ENV: process.env.NODE_ENV || ENV,
   PORT: +process.env.PORT!,
   DB_CONNECTION_URL: process.env.DB_CONNECTION_URL!,
+  NODE_ENV: process.env.NODE_ENV!,
   GEMINI_API_KEY: process.env.GEMINI_API_KEY!,
-  JWT_TOKEN_SECRET: process.env.JWT_TOKEN_SECRET!,
   JWT_TOKEN_EXPIRATION: process.env.JWT_TOKEN_EXPIRATION! as PeriodString,
   REFRESH_TOKEN_EXPIRATION: process.env.REFRESH_TOKEN_EXPIRATION! as PeriodString,
+  JWT_TOKEN_SECRET: process.env.JWT_TOKEN_SECRET!,
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID!,
   GOOGLE_API_KEY: process.env.GOOGLE_API_KEY!,
   GOOGLE_API_BASE_URL: process.env.GOOGLE_API_BASE_URL!,
@@ -26,7 +19,7 @@ export const Env = {
 
 export const verifyEnvVariables = () => {
   Object.entries(Env).forEach(([key, value]) => {
-    if (value === '' || value === undefined || value === null) {
+    if (value === '') {
       throw new Error(`Environment variable ${key} is undefined`);
     }
   });
