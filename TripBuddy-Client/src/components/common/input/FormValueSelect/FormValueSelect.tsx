a34f28776995eed3ad097153e45525005423c2f3
dthv201 <gaya.vishna@gmail.com>
2025-05-14 19:14:57 +0300

    Adding ability to handle moltiple selection in the select form

--- TripBuddy-Client/src/components/common/input/FormValueSelect/FormValueSelect.tsx
+++ TripBuddy-Client/src/components/common/input/FormValueSelect/FormValueSelect.tsx
@@ -1,3 +1,4 @@
+// File: FormValueSelect.tsx
 import {Control, FieldValues, Path, PathValue, useController} from 'react-hook-form';
 import {Typography} from '@mui/joy';
 import {StyledSelect, StyledSelectProps} from '@components/common/input/StyledSelect/StyledSelect';
@@ -5,19 +6,32 @@ import styles from './styles.module.scss';
 
 interface Props<T extends FieldValues = FieldValues> extends Omit<StyledSelectProps, 'onChange' | 'value'> {
   control: Control<T>;
-  formKey: string;
+  formKey: Path<T>;
+  multiple?: boolean;
 }
 
-const FormValueSelect = <T extends FieldValues>({control, formKey, options, ...props}: Props<T>) => {
-  const {field, fieldState} = useController({
+const FormValueSelect = <T extends FieldValues>({control, formKey, options, multiple = false, ...props}: Props<T>) => {
+  const defaultValue = (multiple ? [] : '') as PathValue<T, typeof formKey>;
+
+  const {
+    field: {value, onChange, ...rest},
+    fieldState,
+  } = useController({
     control,
-    name: formKey as Path<T>,
-    defaultValue: '' as PathValue<T, Path<T>>,
+    name: formKey,
+    defaultValue,
   });
 
   return (
     <div className={styles.container}>
-      <StyledSelect options={options} {...field} onChange={value => field.onChange(value)} {...props} />
+      <StyledSelect
+        {...rest}
+        options={options}
+        multiple={multiple}
+        value={value}
+        onChange={val => onChange(val)}
+        {...props}
+      />
       {fieldState.error && (
         <Typography color="danger" level="body-md" fontWeight={700}>
           {fieldState.error.message}
