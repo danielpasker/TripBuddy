234e0b583f5ff49184235b5fe4fa3df97a148c74
Danie Pasker <danielpasker123@gmail.com>
2025-06-06 16:16:57 +0300

    add react compiler for auto memorization

--- TripBuddy-Client/vite.config.ts
+++ TripBuddy-Client/vite.config.ts
@@ -5,5 +5,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler', {}]],
+      },
+    }),
+    tsconfigPaths(),
+    svgr(),
+  ],
 });
